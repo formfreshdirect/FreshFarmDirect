ddb3e69f7d32adfe7ae73238da89a182b9d399b7
DELL <freshformdirect38@gmail.com>
2024-06-25 11:18:56 +0530

    add product changes

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { RetailerRegistrationComponent } from './components/Retailer/retailer-registration/retailer-registration.component';
 import { FarmerRegistrationComponent } from './components/farmer/farmer-registration/farmer-registration.component';
 import { MainComponent } from './components/main/main.component';
+import { AddProductComponent } from './components/product-management/add-product/add-product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'retailer', component: RetailerRegistrationComponent},
   { path: 'farmer', component: FarmerRegistrationComponent},
-  { path: 'main', component: MainComponent},
+  { path: 'main', component: MainComponent },
+  { path: 'addProduct', component: AddProductComponent },
 ];
 
 @NgModule({
